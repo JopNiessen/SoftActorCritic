acc886a6e9d1b1e812e13cf251de2784bc06de11
JopNiessen <jopniessen@gmail.com>
2023-04-01 13:24:56 +0200

    clean up

--- src/utilities/run_trial.py
+++ src/utilities/run_trial.py
@@ -2,25 +2,40 @@
 
 """
 
+import jax.numpy as jnp
 import numpy as np
 
 
-def run(
-    agent,
-    T,
+def run_trial(
+    env,
+    controller,
     x0 = None,
-    n_obs = 2
+    **kwargs
 ):
-    t_space = np.arange(0, T, agent.env.dt)
+    recurrent = kwargs.get('recurrent', False)
+    hidden_size = kwargs.get('hidden_size', 0)
+    T = kwargs.get('T', env.end_time)
 
-    Y = np.zeros((len(t_space), n_obs))
-    S = np.zeros((len(t_space), n_obs))
+    t_space = np.arange(0, T, env.dt)
 
-    y = agent.env.reset(x0)
-    for idx, _ in enumerate(np.arange(0, T, agent.env.dt)):
-        u = agent.actor.model.predict(y)
-        y, _, _, _ = agent.env.step(u)
-        Y[idx] = y
-        S[idx] = agent.env.state
+    y = env.reset(x0)
+    s = env.state
+    u = 0
+    hid = jnp.zeros(hidden_size)
+
+    Y = np.zeros((len(t_space), len(y)))
+    S = np.zeros((len(t_space), len(s)))
+    U = np.zeros((len(t_space), 1))
+
+    for it in range(len(t_space)):
+        if recurrent:
+            u, hid = controller(y, u, hid)
+        else:
+            u = controller(y)
+        y, _, _, _ = env.step(u)
+        Y[it] = y
+        S[it] = env.state
+        U[it] = u
     
-    return S, Y
+    return S, Y, U
+
